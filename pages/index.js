24142736b27066faa6791d8298b32dc93088ebde
Matt Biddulph <mb@hackdiary.com>
2018-11-05 18:38:46 -0800

    react things

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,57 @@
-export default () => <div>Welcome to next.js!</div>;
+import React from 'react';
+import 'isomorphic-fetch';
+import { Lunr } from 'react-lunr';
+import { Formik } from 'formik';
+import search_index from '../static/data/search_index.json';
+import search_store from '../static/data/search_store.json';
+
+const index = JSON.stringify(search_index);
+const store = JSON.stringify(search_store);
+
+export default class extends React.Component {
+  render() {
+    return (
+      <Lunr index={index} store={store}>
+        {({ results, setQuery }) => {
+          console.log(results);
+          return (
+            <>
+              <Formik
+                initialValues={{ query: '' }}
+                onSubmit={(values, { setSubmitting }) => {
+                  setQuery(values.query);
+                  setSubmitting(false);
+                }}
+                render={({
+                  values,
+                  handleSubmit,
+                  handleChange,
+                  handleBlur
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <input
+                      name="query"
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      value={values.query}
+                    />
+                  </form>
+                )}
+              />
+              <h1>Results</h1>
+              <ul>
+                {results.map(result => (
+                  <li key={result.id}>
+                    foo
+                    {result.name}
+                    bar
+                  </li>
+                ))}
+              </ul>
+            </>
+          );
+        }}
+      </Lunr>
+    );
+  }
+}
--- scripts/indexer.js
+++ scripts/indexer.js
@@ -1,6 +1,7 @@
 const Promise = require('bluebird');
 const lunr = require('lunr');
 const fs = Promise.promisifyAll(require('fs'));
+const path = require('path');
 const glob = Promise.promisify(require('glob'));
 
 const builder = new lunr.Builder();
@@ -9,21 +10,47 @@ builder.searchPipeline.add(lunr.stemmer);
 builder.field('name');
 builder.field('description');
 
+const store = {};
+
+const filenameRe = /^([0-9]{10,11})_.*_o.jpg$/;
+const filenameRe2 = /^.*_([0-9]+).*\.jpg$/;
+const storePhoto = filepath => {
+  const jpeg = path.basename(filepath);
+  const id = (filenameRe.exec(jpeg) || filenameRe2.exec(jpeg))[1];
+
+  store[id] = {
+    id,
+    jpeg
+  };
+  return fs.accessAsync(`static/data/photo_${id}.json`); // check related JSON file exists
+};
+
+const saveStore = () =>
+  fs
+    .writeFileAsync(
+      'static/data/search_store.json',
+      JSON.stringify(store, null, 2)
+    )
+    .then(() => 'Store written');
+
 const addPhoto = json => builder.add(JSON.parse(json));
 
 const saveIndex = () =>
   fs
     .writeFileAsync(
-      'data/search_index.json',
+      'static/data/search_index.json',
       JSON.stringify(builder.build().toJSON(), null, 2)
     )
     .then(() => 'Index written');
 
-glob(
-  '/Users/mattb/Pictures/Kathy/72157703094568955_772195d13617_part1/photo_*.json'
-)
+glob('static/data/photo_*.json')
   .then(files =>
     Promise.all(Promise.map(files, f => fs.readFileAsync(f).then(addPhoto)))
   )
   .then(saveIndex)
+  .then(console.log);
+
+glob('static/photos/*.jpg')
+  .then(files => Promise.all(Promise.map(files, storePhoto)))
+  .then(saveStore)
   .then(console.log);
